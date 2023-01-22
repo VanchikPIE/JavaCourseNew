05ad6714d7dc7b7948b0938274b05c9b837c6235
vanek <VanekPIE@bk.ru>
2023-01-22 12:01:04 +0300

    ДЗ_1

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,7 +1,14 @@
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Всем привет");
+       int a = 34;
+        System.out.println(a);
+       float d = 44.4f;
+        System.out.println(d);
+        boolean t = true;
+        System.out.println(t);
+        char name = 'H';
+        System.out.println(name);
+
     }
 
 
